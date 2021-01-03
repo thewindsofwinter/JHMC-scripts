41bef3f5f3bcb7b5e3d4eb0cfaf06ffffc29b3b2
Andy Tang <winterwind2022@gmail.com>
2021-01-02 19:17:24 -0600

    commit initial script

---
+++ main.js
@@ -0,0 +1,39 @@
+const AirtablePlus = require('airtable-plus');
+const DotEnv = require('dotenv').config();
+
+// baseID, apiKey, and tableName can alternatively be set by environment variables
+const airtable = new AirtablePlus({
+    apiKey: process.env.AIRTABLE_API_KEY,
+    baseID: process.env.AIRTABLE_BASE_ID,
+    tableName: process.env.AIRTABLE_TABLE_NAME,
+});
+
+const result = {
+  "recordId": "reciWTsrkj3xzZPVJ",
+  "answers": [
+    {"Q1": "10"},
+    {"Q2": "3.14159"},
+    {"Q3": "1"},
+    {"Q4": "4"},
+    {"Q5": "1"},
+    {"Q6": "5"},
+    {"Q7": "9"},
+    {"Q8": "3.12"},
+    {"Q9": "119"},
+    {"Q10": "0"}
+  ]
+};
+
+(async () => {
+    try {
+        const id = result.recordId;
+
+        const time = await airtable.update(id, {"Submission Time": Date.now()})
+        for(const val in result.answers) {
+            const res = await airtable.update(id, result.answers[val]);
+        }
+    }
+    catch(e) {
+        console.error(e);
+    }
+})();
