2468e7a9b3f4cde489f4cebca925498a47f7ebd1
Andy Tang <winterwind2022@gmail.com>
2021-01-17 12:03:54 -0600

    import CSV parsing utilities

--- one-time-scripts/load-students.js
+++ one-time-scripts/load-students.js
@@ -1,7 +1,21 @@
 const AirtablePlus = require('airtable-plus');
+const Csv = require('csvtojson');
 const DotEnv = require('dotenv').config({ path: './../.env' });
 
 const testsTable = new AirtablePlus({ tableName: "Tests" }),
     studentsTable = new AirtablePlus({ tableName: "Students" }),
     schoolsTable = new AirtablePlus({ tableName: "Schools" }),
     competitionsTable = new AirtablePlus({ tableName: "Competitions" });
+
+// Change once we get actual rosters
+const csvFilePath='./../sampleRoster.csv'
+
+Csv().fromFile(csvFilePath).then((jsonObj)=>{
+        console.log(jsonObj);
+        /**
+         * [
+         * 	{a:"1", b:"2", c:"3"},
+         * 	{a:"4", b:"5". c:"6"}
+         * ]
+         */
+    })
