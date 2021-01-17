ae92b57fd73b6cc4a08b3f4d9704963e5526b633
Andy Tang <winterwind2022@gmail.com>
2021-01-17 11:54:42 -0600

    create script to load students

---
+++ one-time-scripts/load-students.js
@@ -0,0 +1,8 @@
+const AirtablePlus = require('airtable-plus');
+const DotEnv = require('dotenv').config();
+
+
+const testsTable = new AirtablePlus({ tableName: "Tests" }),
+    studentsTable = new AirtablePlus({ tableName: "Students" }),
+    schoolsTable = new AirtablePlus({ tableName: "Schools" }),
+    competitionsTable = new AirtablePlus({ tableName: "Competitions" });
