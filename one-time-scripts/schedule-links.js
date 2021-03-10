21d6fdd473b7e00d6c4104403b4c1d0e9aa6b6a5
Patrick Hultquist <patrick@patrick.today>
2021-03-10 00:45:05 -0600

    schedule links

---
+++ one-time-scripts/schedule-links.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const AirtablePlus = require('airtable-plus');
+const DotEnv = require('dotenv').config({ path: './../.env' });
+const fs = require("fs");
+const { mdToPdf } = require('md-to-pdf');
+
+const saveAllPdfs = true;
+
+// baseID, apiKey, and tableName can alternatively be set by environment variables
+const testsTable = new AirtablePlus({ tableName: "Tests" }),
+    studentsTable = new AirtablePlus({ tableName: "Students" }),
+    schoolsTable = new AirtablePlus({ tableName: "Schools" }),
+    competitionsTable = new AirtablePlus({ tableName: "Competitions" });
+
+const generateJoinLinks = async () => {
+    try {
+        const schools = await schoolsTable.read();
+
+        let studentLinks;
+        let finalMd = ""
+        schools.forEach(async school => {
+            let schoolName = school.fields["Name"];
+            let studentNames = school.fields["Student Names"];
+            let studentLinks = school.fields["Student Links"];
+            let students = [];
+            studentNames.forEach((studentName, i) => {
+                students.push({
+                    name: studentName,
+                    link: studentLinks[i]
+                });
+            });
+
+            let studentLinkTexts = students.map(s => s.name + ": "+`[${s.link}](${s.link})`).join(" \\\n");
+            let schoolMd = `# ${schoolName}\n## Coached By ${school.fields["Coach Name"]}\n### Division ${school.fields["Division"]}\n${studentLinkTexts}\n<div style="page-break-after: always;"></div>\n\n`;
+            console.log(schoolMd);
+            // console.log(studentLinks);
+            await mdToPdf({ content: schoolMd }, { dest: `final/${schoolName}.pdf` });
+
+            finalMd += schoolMd;
+        });
+        console.log(finalMd);
+        if (saveAllPdfs) {
+            // for some reason, this line doesn't work for me. leaving it here for the future if someone wants a list of all schools.
+            // await mdToPdf({ content: finalMd }, { dest: `schedule-links.pdf` });
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+generateJoinLinks();
