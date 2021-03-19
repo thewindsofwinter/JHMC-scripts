71b65a1fa81251d5af2fe9d65afd14ef10158cce
phultquist <phultquist@imsa.edu>
2021-03-18 22:55:43 -0500

    attempting to run cloud functions

--- index.js
+++ index.js
@@ -253,9 +253,8 @@ process.on('unhandledRejection', (reason, p) => {
     // application specific logging, throwing an error, or other logic here
 });
 
-const server = http.listen(8080, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-
-    console.log(`App listening at http://localhost:${port}`);
+app.listen(8080, () => {
+    console.log(`Server running on port http://localhost:${8080}`);
 });
+
+exports.app = app;
