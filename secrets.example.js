39fcc58d5e9c1fa9e54383bd1ed1367057fbc320
Vidyoot Senthil <vidyoots@gmail.com>
2024-01-30 22:15:21 -0600

    vid update

--- secrets.example.js
+++
@@ -1,8 +0,0 @@
-// Rename this file to secrets.js and make sure it's in your .gitignore.
-
-module.exports = {
-    apiKey: "apikey", // API Key for Airtable
-    baseID: "baseid", // Base ID for Airtable
-    sampleTestId: "sampletestid", // ID for the sample test, given special permissions
-    masterTestId: "mastertestid" // ID for the master test, also given special permissions but not public
-}
